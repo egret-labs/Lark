e68f257853017628da742bbcdb0e4ad515c09f30
zcxp <zc668xp@gmail.com>
2015-03-26 20:33:39 +0800

    添加媒体类

---
+++ src/Media.ts
@@ -0,0 +1,15 @@
+﻿
+
+module lark {
+    export class Media extends DisplayObjectContainer {
+        constructor() {
+            super();
+
+            var audio = LarkAudio.create("sound/mp3.mp3");
+            var sources = ["sound/ogg.ogg", "sound/wav.wav", "sound/weba.weba", "sound/opus.upus", "sound/flac.flac", "sound/mp3.mp3"]
+            audio.sources = sources.map(s=> { return { src: s } });
+            audio.load();
+            audio.play();
+        }
+    }
+}
---
+++ src/extension/media/Media.ts
@@ -0,0 +1,44 @@
+﻿module lark {
+
+    export interface IMediaSource {
+        src: string;
+        mimeType?: string;
+    }
+
+    export class LarkMedia extends EventDispatcher {
+        public constructor(src?:string,mimeType?:string) {
+            super();
+            this.sources = src ? [{ src: src, mimeType: mimeType }] : [];
+        }
+        
+        public sources: IMediaSource[];
+
+        public get volume(): number {
+            return this.getVolume();
+        }
+
+        public set volume(value: number) {
+            this.setVolume(value);
+        }
+
+        protected getVolume(): number {
+            return 0;
+        }
+
+        protected setVolume(value: number) {
+
+        }
+
+        public load() {
+
+        }
+
+        public play(loop: boolean = false) {
+
+        }
+
+        public pause() {
+
+        }
+    }
+}
