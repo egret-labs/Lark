6824e7f04a2e11fbd9a2a00b8e886c90e31b2679
zcxp <zc668xp@gmail.com>
2015-04-02 11:20:33 +0800

    添加设备兼容性类

---
+++ src/extension/sensers/GeoLocation.ts
@@ -0,0 +1,18 @@
+﻿
+
+module lark {
+    export class GeoLocation extends EventEmitter {
+        protected geolocation: Geolocation = null;
+        constructor() {
+            super();
+            this.geolocation = navigator.geolocation;
+        }
+
+        public getLocation() {
+            var geo = this.geolocation;
+            geo.getCurrentPosition(position=> {
+                console.log(position);
+            }, error=> { });
+        }
+    }
+}
