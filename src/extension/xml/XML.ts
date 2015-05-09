11b90f95c6955f57df59016d5a8f01238e606ded
dom <dom@domlib.com>
2015-05-09 16:04:47 +0800

    move XML classes to extension package

--- src/extension/xml/XML.ts
+++ src/extension/xml/XML.ts
@@ -28,68 +28,52 @@
 //////////////////////////////////////////////////////////////////////////////////////
 
 module lark {
-
-    /**
-     * XML节点基类
-     */
-    export interface XMLNode {
-        /**
-         * 节点类型，1：XML，2：XMLAttribute，3：XMLText
-         */
-        nodeType:number;
-        /**
-         * 节点所属的父级节点
-         */
-        parentNode: XML;
-    }
-
     /**
      * XML节点对象
      */
-    export interface XML extends XMLNode {
+    export class XML{
+
+        /**
+         * 解析字符串为XML对象
+         * @param text 要解析的字符串
+         */
+        public static parse:(text:string)=>XML;
+
+        public constructor(localName:string, parentNode:XML, prefix:string, namespaceURI:string, nodeName:string) {
+            this.parentNode = parentNode;
+            this.localName = localName;
+            this.prefix = prefix;
+            this.namespaceURI = namespaceURI;
+            this.nodeName = nodeName;
+        }
+        /**
+         * 节点所属的父级节点
+         */
+        public parentNode:XML;
         /**
          * 当前节点上的属性列表
          */
-        attributes:XMLAttribute[];
+        public attributes:XMLAttribute[] = [];
         /**
          * 当前节点的子节点列表
          */
-        childNodes:(XML|XMLText)[];
+        public childNodes:(XML|XMLText)[] = [];
         /**
          * 节点完整名称。例如节点 <e:Button/> 的 nodeName 为：e:Button
          */
-        nodeName:string;
+        public nodeName:string;
         /**
          * 节点的命名空间前缀。例如节点 <e:Button/> 的 prefix 为：e
          */
-        prefix: string;
+        public prefix:string;
         /**
          * 节点的本地名称。例如节点 <e:Button/> 的 prefix 为：Button
          */
-        localName:string;
+        public localName:string;
         /**
          * 节点的命名空间地址。例如节点 <e:Skin xmlns:e="http://ns.egret-labs.org/egret"/> 的 namespaceURI 为： http://ns.egret-labs.org/egret
          */
-        namespaceURI: string;
-    }
-
-    /**
-     * XML文本节点
-     */
-    export interface XMLText extends XMLNode {
-        data:string;
-    }
-    /**
-     * XML属性节点
-     */
-    export interface XMLAttribute extends XMLNode {
-        /**
-         * 属性名称
-         */
-        name: string;
-        /**
-         * 属性值
-         */
-        value:string;
+        public namespaceURI:string;
     }
 }
+
