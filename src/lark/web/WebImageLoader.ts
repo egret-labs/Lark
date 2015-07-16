f55352e35bf572cddbdcd7a0f63d28a2d7d6eed6
dom <dom@domlib.com>
2015-07-16 17:20:02 +0800

    fix the cross origin problem in IOS when use the ImageLoader to load an external image

--- src/lark/web/WebImageLoader.ts
+++ src/lark/web/WebImageLoader.ts
@@ -71,7 +71,9 @@ module lark.web {
          * @param url 要加载的图像文件的地址。
          */
         public load(url:string):void {
-            if (useXHR&&url.indexOf("data:")!=0) {//如果是base64编码图片，直接使用Image.src解析。
+            if (useXHR&&url.indexOf("data:")!=0&&
+                url.indexOf("http:") != 0 &&
+                url.indexOf("https:") != 0) {//如果是base64编码或跨域访问的图片，直接使用Image.src解析。
                 var request = this.request;
                 if (!request) {
                     request = this.request = new lark.web.WebHttpRequest();
